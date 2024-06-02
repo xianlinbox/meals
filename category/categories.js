fc485f12dc68fdd7f92a49d6011ab08a9dfd5bc5
xianning liu <xianlinbox@gmail.com>
2024-06-02 18:44:43 -0500

    add style for category gridn

--- category/categories.js
+++ category/categories.js
@@ -3,12 +3,13 @@ import { CATEGORIES } from "../models/dummy-data";
 import CategoryTile from "./components/category-tile";
 
 function CategoryScreen() {
+  function showMeals() {}
   return (
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
       renderItem={(itemData) => {
-        return <CategoryTile title={itemData.item.title} color={itemData.item.color} />;
+        return <CategoryTile title={itemData.item.title} color={itemData.item.color} onPress={showMeals} />;
       }}
       numColumns={2}
     />
--- category/components/category-tile.js
+++ category/components/category-tile.js
@@ -1,6 +1,44 @@
-import { Text } from "react-native";
-function CategoryTile({ title, color }) {
-  return <Text>{title}</Text>;
+import { Pressable, Text, View, StyleSheet, Platform } from "react-native";
+function CategoryTile({ title, color, onPress }) {
+  return (
+    <View style={styles.categoryTile}>
+      <Pressable
+        style={({ pressed }) => (pressed ? [styles.pressable, styles.pressed] : styles.pressable)}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.text}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 }
 
 export default CategoryTile;
+
+const styles = StyleSheet.create({
+  categoryTile: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  pressable: {
+    flex: 1,
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+  innerContainer: { flex: 1, padding: 16, borderRadius: 8, justifyContent: "center", alignItems: "center" },
+  text: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
