c5506663da98a4d0776c98ed8116f11f01eb301b
xianning liu <xianlinbox@gmail.com>
2024-06-03 18:59:15 -0500

    add image and title for meal detail" "

--- meals/meal-detail.js
+++ meals/meal-detail.js
@@ -1,17 +1,23 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
 import { MEALS } from "../models/dummy-data";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
-  const meal = MEALS.filter((meal) => meal.id == mealId)[0];
+  const meal = MEALS.find((meal) => meal.id == mealId);
 
   return (
-    <View>
-      <Text>{meal.title}</Text>
-    </View>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: meal.imageUrl }} />
+      <Text style={styles.title}>{meal.title}</Text>
+      <View></View>
+    </ScrollView>
   );
 }
 
 export default MealDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: { marginBottom: 32 },
+  image: { width: "100%", height: 350 },
+  title: { fontWeight: "bold", fontSize: 24, margin: 8, textAlign: "center", color: "white" },
+});
