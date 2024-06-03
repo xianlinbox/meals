4f1f058ca482f79e921163dfd1b0ca7c456533a4
xianning liu <xianlinbox@gmail.com>
2024-06-03 18:38:15 -0500

    add router for Meal Detail

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoryScreen from "./category/categories";
 import MealsOverviewScreen from "./meals/meals-overview";
+import MealDetailScreen from "./meals/meal-detail";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="categories">
-        <Stack.Screen name="categories" component={CategoryScreen} />
-        <Stack.Screen name="meals-overview" component={MealsOverviewScreen} />
+        <Stack.Screen name="categories" options={{ title: "All Categories" }} component={CategoryScreen} />
+        <Stack.Screen name="meals-overview" options={{ title: "Meals" }} component={MealsOverviewScreen} />
+        <Stack.Screen name="meal-detail" options={{ title: "Meal Detail" }} component={MealDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ meals/meal-detail.js
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from "react-native";
+
+function MealDetailScreen() {
+  return <View></View>;
+}
+
+export default MealDetailScreen;
+
+const styles = StyleSheet.create({});
