0210598cf6cd3f4ae3722a026365934b9def1b8a
xianning liu <xianlinbox@gmail.com>
2024-06-04 17:12:00 -0500

    add meal details screen

--- meals/meal-detail.js
+++ meals/meal-detail.js
@@ -1,15 +1,29 @@
 import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
+import Subtitle from "./components/subtitle";
+import List from "./components/list";
 import { MEALS } from "../models/dummy-data";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
   const meal = MEALS.find((meal) => meal.id == mealId);
-
+  const { duration, complexity, affordability } = meal;
   return (
     <ScrollView style={styles.rootContainer}>
       <Image style={styles.image} source={{ uri: meal.imageUrl }} />
       <Text style={styles.title}>{meal.title}</Text>
-      <View></View>
+      <View style={styles.details}>
+        <Text style={styles.detailItem}>{duration}m</Text>
+        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+      </View>
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={meal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={meal.steps} />
+        </View>
+      </View>
     </ScrollView>
   );
 }
@@ -20,4 +34,21 @@ const styles = StyleSheet.create({
   rootContainer: { marginBottom: 32 },
   image: { width: "100%", height: 350 },
   title: { fontWeight: "bold", fontSize: 24, margin: 8, textAlign: "center", color: "white" },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
 });
