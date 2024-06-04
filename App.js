48289ea658c6d67851cd543f3d5530e071a070a9
xianning liu <xianlinbox@gmail.com>
2024-06-03 20:01:58 -0500

    set background color

--- App.js
+++ App.js
@@ -3,16 +3,27 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoryScreen from "./category/categories";
 import MealsOverviewScreen from "./meals/meals-overview";
 import MealDetailScreen from "./meals/meal-detail";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="categories">
-        <Stack.Screen name="categories" options={{ title: "All Categories" }} component={CategoryScreen} />
-        <Stack.Screen name="meals-overview" options={{ title: "Meals" }} component={MealsOverviewScreen} />
-        <Stack.Screen name="meal-detail" options={{ title: "Meal Detail" }} component={MealDetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="categories"
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen name="categories" options={{ title: "All Categories" }} component={CategoryScreen} />
+          <Stack.Screen name="meals-overview" options={{ title: "Meals" }} component={MealsOverviewScreen} />
+          <Stack.Screen name="meal-detail" options={{ title: "Meal Detail" }} component={MealDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
