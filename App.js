42c69a6531d8772066fbdf6d46acb6fa9baa8410
xianning liu <xianlinbox@gmail.com>
2024-06-06 19:14:59 -0600

    add drawer navigation

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StatusBar } from "react-native";
 import CategoryScreen from "./category/categories";
 import MealsOverviewScreen from "./meals/meals-overview";
 import MealDetailScreen from "./meals/meal-detail";
-import { StatusBar } from "react-native";
+import FavoriteScreen from "./favorite/favorite-screen";
+const Drawer = createDrawerNavigator();
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="All Categories" component={CategoryScreen} />
+      <Drawer.Screen name="Favorites" component={FavoriteScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+
   return (
     <>
       <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="categories"
           screenOptions={{
             headerStyle: { backgroundColor: "#351401" },
             headerTintColor: "white",
             contentStyle: { backgroundColor: "#3f2f25" },
           }}
         >
-          <Stack.Screen name="categories" options={{ title: "All Categories" }} component={CategoryScreen} />
+          <Stack.Screen name="drawer" component={DrawerNavigator} options={{ headerShown: false }} />
           <Stack.Screen name="meals-overview" options={{ title: "Meals" }} component={MealsOverviewScreen} />
           <Stack.Screen name="meal-detail" options={{ title: "Meal Detail" }} component={MealDetailScreen} />
         </Stack.Navigator>
---
+++ favorite/favorite-screen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function FavoriteScreen() {
+  return (
+    <View>
+      <Text>This is the favorite screen</Text>
+    </View>
+  );
+}
+
+export default FavoriteScreen;
