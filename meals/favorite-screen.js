c3ca6f17472eedb058e0b82ee5602631a3bb91a4
xianning liu <xianlinbox@gmail.com>
2024-06-15 19:29:36 -0500

    show favorite meals'

--- meals/favorite-screen.js
+++ meals/favorite-screen.js
@@ -1,17 +1,13 @@
 import { Text, View } from "react-native";
 import { useContext } from "react";
 import { FavoritesContext } from "../store/favorite-context";
-import MealCard from "./components/meal-card";
+import MealList from "./components/meal-list";
 import { MEALS } from "../models/dummy-data";
 function FavoriteScreen() {
   const favoriteMealsContext = useContext(FavoritesContext);
   const favoriteMeals = MEALS.filter((meal) => favoriteMealsContext.mealIds.includes(meal.id));
 
-  return (
-    <View>
-      <Text>This is the favorite screen</Text>
-    </View>
-  );
+  return <MealList meals={favoriteMeals} />;
 }
 
 export default FavoriteScreen;
